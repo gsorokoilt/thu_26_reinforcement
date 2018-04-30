0977fd707b7f0eb373c47402ab5e5c6b900d99f5
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-04-30 18:07:09 -0400

    no change

--- reinforcement.rb
+++ reinforcement.rb
@@ -16,4 +16,4 @@ puts find_record_with_most_points(records)
 
 #in above example, method takes array as argument,
 #referring to each hash in the array as |hash|
-#max_by then compares value of :points from each hash and puts the hash with the highest :points value
+#max_by then compares value of :points from each hash and returns the hash with the highest :points value
